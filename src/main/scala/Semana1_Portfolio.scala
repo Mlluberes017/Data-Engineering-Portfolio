a82be8641d3f3e0b7c59ac8e2aa63c76ad295b6f
Marla Lluberes <marlalluberes017@gmail.com>
2026-02-07 17:30:59 -0400

    Initial commit – Semana 1 Scala/Spark

---
+++ src/main/scala/Semana1_Portfolio.scala
@@ -0,0 +1,7 @@
+object Semana1_Portfolio {
+  def main(args: Array[String]): Unit = {
+    println("Hola! Este es mi portafolio de Scala y Spark (EOI).")
+    println("Semana 1: Scala y Programación Funcional.")
+  }
+}
+
